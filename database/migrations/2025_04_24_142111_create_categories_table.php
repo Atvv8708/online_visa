192a53d439dc8ddb609d2485db0aec514872c5b2
Atvv <atayevj13@gmail.com>
2025-04-24 19:25:50 +0500

    1.0.1

--- database/migrations/2025_04_24_142111_create_categories_table.php
+++ database/migrations/2025_04_24_142111_create_categories_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('questions', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->text('description');
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('questions');
+        Schema::dropIfExists('categories');
     }
 };
